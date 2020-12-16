fb79560b11b9a9851fa4eb656d6c1fe406bb3984
Tom <macos2@21.com>
2020-12-16 17:49:30 +0800

    fix ds1302

--- gpio.c
+++ gpio.c
@@ -14,10 +14,6 @@
 					 	case 0x##i##4:result=P##i##4;break;case 0x##i##5:result=P##i##5;break;case 0x##i##6:result=P##i##6;break;case 0x##i##7:result=P##i##7;break;\
 					 	case 0x##i##F:result=P##i;break;
 
-//#define GET_PORT(x) case x: return P##x;break;
-//#define GET_PIN(x)  case x: switch (port){ case 0:return P0##x;break; case 1:return P1##x;break; case 2:return P2##x;break; case 3:return P3##x;break; case 4:return P4##x;break;}break;
-
-//this is a test
 void gpio_set(gpio io,unsigned char value) {
 	switch (io){
 	SET_IO(0)
@@ -29,54 +25,6 @@ void gpio_set(gpio io,unsigned char value) {
 	}
 	__asm__("RET");
 }
-
-//void gpio_set(gpio io,unsigned char value) {
-//	unsigned char port=io>>4;
-//	unsigned char pin=io&0x0f;
-//	switch(pin){
-//		case GPIO_ALL_PIN:
-//			switch(port){
-//				SET_PORT(0);
-//				SET_PORT(1);
-//				SET_PORT(2);
-//				SET_PORT(3);
-//				SET_PORT(4);
-//			}
-//			break;
-//		SET_PIN(0)
-//		SET_PIN(1)
-//		SET_PIN(2)
-//		SET_PIN(3)
-//		SET_PIN(4)
-//		SET_PIN(5)
-//		SET_PIN(6)
-//		SET_PIN(7)
-//	};
-//}
-
-//unsigned char gpio_get(gpio io) {
-//	unsigned char port=io>>4;
-//	unsigned char pin=io&0x0f;
-//	switch (pin){
-//		case  GPIO_ALL_PIN:
-//			switch(port){
-//				GET_PORT(0)
-//				GET_PORT(1)
-//				GET_PORT(2)
-//				GET_PORT(3)
-//				GET_PORT(4)
-//			}break;
-//		GET_PIN(0)
-//		GET_PIN(1)
-//		GET_PIN(2)
-//		GET_PIN(3)
-//		GET_PIN(4)
-//		GET_PIN(5)
-//		GET_PIN(6)
-//		GET_PIN(7)
-//	}
-//	return 0x00;
-//}
 
 unsigned char gpio_get(gpio io){
 	unsigned char result=0x00;
