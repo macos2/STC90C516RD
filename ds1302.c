478cac8141be1957066c506daf395eb4f312ac4d
Tom <macos2@21.com>
2020-12-16 17:28:23 +0800

    fix ds1302 problem

--- ds1302.c
+++ ds1302.c
@@ -31,34 +31,35 @@ void ds1302_write(Ds1302 *dev, unsigned is_ram_data, unsigned char addr,
 	gpio_set(dev->rst, 0);
 }
 
-void ds1302_read(Ds1302 *dev, unsigned is_ram_data,	unsigned char addr, unsigned char *result) {
-	unsigned char command = 0x81, i;
+unsigned char ds1302_read(Ds1302 *dev, unsigned is_ram_data, unsigned char addr) {
+	unsigned char command = 0x81, i,result=0x00;
 	if (is_ram_data)
 		command += 0x40;
 	command |= (addr & 0x1F) << 1;
-
-		gpio_set(dev->rst, 0);
-		gpio_set(dev->io, 0);
+	gpio_set(dev->rst, 0);
+	gpio_set(dev->io, 0);
+	gpio_set(dev->sclk, 0);
+	gpio_set(dev->rst, 1);
+	for (i = 0; i < 8; i++) {
 		gpio_set(dev->sclk, 0);
-		gpio_set(dev->rst, 1);
-		command |= (addr & 0x1F) << 1;
-		for (i = 0; i < 8; i++) {
-			gpio_set(dev->sclk, 0);
-			gpio_set(dev->io, 0x01 & (command >> i));
-			gpio_set(dev->sclk, 1);
 
-		}
-		for (i = 0; i < 8; i++) {
-			gpio_set(dev->sclk, 0);
-			*result |= (gpio_get(dev->io) << i);
-			gpio_set(dev->sclk, 1);
-		}
-		gpio_set(dev->rst, 0);
+		gpio_set(dev->io, 0x01 & (command >> i));
+		gpio_set(dev->sclk, 1);
+
 	}
-
+	for (i = 0; i < 8; i++) {
+		gpio_set(dev->sclk, 0);
+		 command=gpio_get(dev->io);
+		result |= ( command<< i);
+		gpio_set(dev->sclk, 1);
+	}
+	gpio_set(dev->rst, 0);
+	return result;
+}
 
 unsigned char __tmp;
-void hex2bcd() __naked {
+void hex2bcd()
+__naked {
 	__asm
 	MOV A,___tmp
 	DA A
@@ -69,31 +70,30 @@ void hex2bcd() __naked {
 
 void ds1302_set_date(Ds1302 *dev, unsigned char year, unsigned char month,
 		unsigned char day) {
-	__tmp=day;
+	__tmp = day;
 	hex2bcd();
-	ds1302_write(dev,0,0x03,__tmp);//day
+	ds1302_write(dev, 0, 0x03, __tmp); //day
 
-	__tmp=month;
+	__tmp = month;
 	hex2bcd();
-	ds1302_write(dev,0,0x04,__tmp);//month
+	ds1302_write(dev, 0, 0x04, __tmp); //month
 
-	__tmp=year;
+	__tmp = year;
 	hex2bcd();
-	ds1302_write(dev,0,0x06,__tmp);//year
-
+	ds1302_write(dev, 0, 0x06, __tmp); //year
 
 }
 
 void ds1302_set_time(Ds1302 *dev, unsigned char hour, unsigned char minute,
 		unsigned char second) {
-	__tmp=second;
+	__tmp = second;
 	hex2bcd();
-	ds1302_write(dev,0,0x00,__tmp);//second
-	__tmp=minute;
+	ds1302_write(dev, 0, 0x00, __tmp); //second
+	__tmp = minute;
 	hex2bcd();
-	ds1302_write(dev,0,0x01,__tmp);//minute
-	__tmp=hour;
+	ds1302_write(dev, 0, 0x01, __tmp); //minute
+	__tmp = hour;
 	hex2bcd();
-	ds1302_write(dev,0,0x02,__tmp);//hour
+	ds1302_write(dev, 0, 0x02, __tmp); //hour
 
 }
