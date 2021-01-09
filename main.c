45e1eb5bd08b0e9950266280aa9ad9b5ae88e984
Tom <macos2@21.com>
2021-01-09 17:49:54 +0800

    nothing

--- main.c
+++ main.c
@@ -9,8 +9,8 @@
 
 __xdata unsigned int timer0;
 __xdata unsigned char usart_buf[128], usart_p, usart_e, i2c_dev[10],timer0_isp;
-__xdata I2cBus iic;
-__xdata I2cMemory i2c_mem;
+//__xdata I2cBus iic;
+//__xdata I2cMemory i2c_mem;
 __xdata SpiBus spi;
 //__xdata SpiMemory spi_mem;
 __xdata SpiSd spi_sd;
@@ -49,12 +49,15 @@ __interrupt 4 {
 		RI=0;
 		t=SBUF;
 		SBUF=t;
+		if(t=='1')test=1;
+		if(t=='2')test=2;
+		if(t=='3')test=3;
+		if(t=='4')test=4;
 		//iic_test();
 		//iic_eeprom();
 		//spi_test();
 		//long_t();
 		//usart_send("received 0x%02x\r\n",t);
-		test++;
 	} else {
 		TI=0;
 		if(usart_p>0) {
@@ -86,7 +89,7 @@ void usart_init() {
 //	AUXR &= 0xBF;//定时器1时钟为Fosc/12,即12T
 //	AUXR &= 0xFE;//串口1选择定时器1为波特率发生器
 	P3M0=0x03;
-	P3M1=0x01;
+	P3M1=0x00;
 //	TH1 = 253;
 //	TL1 = 253;
 //	TR1 = 1;
@@ -107,51 +110,51 @@ void timer0_init() {
 	timer0_isp=0;
 }
 
-void iic_init() {
+//void iic_init() {
+//
+//	iic.sck = gpio_format(1, 0);
+//	iic.sda = gpio_format(1, 1);
+//	i2c_mem.bus = &iic;
+//	i2c_mem.dev_addr = 0x50;
+//	i2c_mem.page_size = 8;
+//	i2c_mem.n_bit_addr = I2C_MEMORY_8_BIT_ADDRESS;
+//	//iic_test();
+//	//iic_eeprom();
+//}
 
-	iic.sck = gpio_format(1, 0);
-	iic.sda = gpio_format(1, 1);
-	i2c_mem.bus = &iic;
-	i2c_mem.dev_addr = 0x50;
-	i2c_mem.page_size = 8;
-	i2c_mem.n_bit_addr = I2C_MEMORY_8_BIT_ADDRESS;
-	//iic_test();
-	//iic_eeprom();
-}
-
-void iic_test() {
-	unsigned char i, n;
-	n = i2c_scan_7bit_addr_dev(&iic, i2c_dev, 10);
-	usart_send("\r\nGet i2c dev addr: ");
-	for (i = 0; i < n; i++) {
-		usart_send("0x%02x ", i2c_dev[i]);
-	}
+//void iic_test() {
+//	unsigned char i, n;
+//	n = i2c_scan_7bit_addr_dev(&iic, i2c_dev, 10);
+//	usart_send("\r\nGet i2c dev addr: ");
+//	for (i = 0; i < n; i++) {
+//		usart_send("0x%02x ", i2c_dev[i]);
+//	}
 //	usart_send("\r\n");
 	//get 0x48 0x50 0x51 0x52 0x53 0x54 0x55 0x56 0x57
-}
+//}
 
-void iic_eeprom() {
-	unsigned char i, buf[25];
-	for (i = 0; i < 25; i++) {
-		buf[i] = i;
-	}
-	i2c_memory_set_rw_param(&i2c_mem, 0x08, 25, buf);
-	i = i2c_memory_write();
-	if (i != 25)
-		usart_send("write i2c memory fail\r\n");
-	for (i = 0; i < 25; i++) {
-		buf[i] = 0;
-	}
-	i2c_memory_set_rw_param(&i2c_mem, 0x08, 25, buf);
-	i = i2c_memory_read();
-	if (i != 25)
-		usart_send("read i2c memory fail\r\n");
-	usart_send("\r\nbuf:\r\n");
-	for (i = 0; i < 25; i++) {
-		usart_send("%02x ", i, buf[i]);
-	}
-	usart_send("\r\n");
-}
+//void iic_eeprom() {
+//	unsigned char i, buf[25];
+//	for (i = 0; i < 25; i++) {
+//		buf[i] = i;
+//	}
+//	i2c_memory_set_rw_param(&i2c_mem, 0x08, 25, buf);
+//	i = i2c_memory_write();
+//	if (i != 25)
+//		usart_send("write i2c memory fail\r\n");
+//	for (i = 0; i < 25; i++) {
+//		buf[i] = 0;
+//	}
+//	i2c_memory_set_rw_param(&i2c_mem, 0x08, 25, buf);
+//	i = i2c_memory_read();
+//	if (i != 25)
+//		usart_send("read i2c memory fail\r\n");
+//	usart_send("\r\nbuf:\r\n");
+//	for (i = 0; i < 25; i++) {
+//		usart_send("%02x ", i, buf[i]);
+//	}
+//	usart_send("\r\n");
+//}
 
 void spi_main_init() {
 	spi.cpha = 0;
@@ -191,13 +194,13 @@ void spi_test() {
 #define test_crc7(d,c) 	i=crc7_calc(0x##d);i=crc7_calc_end(i<<7);usart_send("crc7("#d")=%02x,0x"#c"\r\n",i)
 #define test_crc16(d) j=crc16_calc(d);j=crc16_calc(d|j);if(j==0)usart_send("\r\nPass\r\n");else usart_send("\r\nError\r\n");
 void main() {
-
-	unsigned int i,j;
+	unsigned char i;
+	unsigned int j;
 //	unsigned long j;
 	gpio io = gpio_format(1, 7);
 	usart_init();
 	timer0_init();
-	iic_init();
+	//iic_init();
 	spi_main_init();
 	test=0;
 	//iic_test();
@@ -242,7 +245,7 @@ void main() {
 	while (1) {
 		gpio_set(io, 0);
 		gpio_set(io, 1);
-		while(test>0){
+		if(test==1){
 			spi_sd_init(&spi_sd,64,1);
 //			spi_sd_read(&spi_sd,0,sd_buf,2);
 //			for(i=0;i<128;i++){
@@ -252,22 +255,40 @@ void main() {
 //			spi_sd_write(&spi_sd,64,sd_buf+64,1);
 			spi_sd_set_rw_param(&spi_sd,0,sd_buf,1);
 			spi_sd_read();
-			usart_send("read buffer:\r\n");
-			for(i=0;i<spi_sd.block_size;i++){
-				usart_send("%02x ",sd_buf[i]);
-			}
 			usart_send("\r\n");
 			spi_sd_set_rw_param(&spi_sd,0,sd_buf,1);
 			spi_sd_write();
 			test=0;
-			i=one_wire_bus_search_rom(0,sd_buf,5);
-			usart_send("%d device detected\r\n",i);
-			j=0;
-			while(i>0){
-				usart_send("%d %02x %02x %02x %02x %02x %02x %02x",sd_buf[j],sd_buf[j+1],sd_buf[j+2],sd_buf[j+3],sd_buf[j+4],sd_buf[j+5]);
-				j+=6;
-				i--;
+
+		}
+		if(test==2){
+			usart_send("read buffer:\r\n");
+			for(i=0;i<255;i++){
+				usart_send("%02x ",sd_buf[i]);
 			}
+			test=0;
+		}
+		if(test==3){
+			i=one_wire_bus_search_rom(0,sd_buf,5);
+					usart_send("%d device detected\r\n",i);
+					j=0;
+					while(i>0){
+						usart_send("%d %02x %02x %02x %02x %02x %02x %02x",sd_buf[j],sd_buf[j+1],sd_buf[j+2],sd_buf[j+3],sd_buf[j+4],sd_buf[j+5]);
+						j+=6;
+						i--;
+					}
+					test=0;
+
+		}
+		if(test==4){
+			for(i=0;i<255;i++){
+				sd_buf[i]=i;
+			}
+			usart_send("read buffer:\r\n");
+			for(i=0;i<255;i++){
+				usart_send("%02x ",sd_buf[i]);
+			}
+			test=0;
 		}
 //		i2c_start(&iic);
 //		i2c_send_7bit_addr(&iic,0x46,0);
--- makefile
+++ makefile
@@ -1,10 +1,10 @@
-opt= -mmcs51  --model-large #--model-large #--model-huge # --model-large #--model-medium # -DUSE_FLOATS=1
+opt= -mmcs51  --model-medium #--model-large #--model-huge # --model-large #--model-medium # -DUSE_FLOATS=1
 all:main.hex
 
 clean:
 	rm *.rel *.rst *.lst *.sym *.map *.mem *.ihx *.hex *.lk *.asm
 
-main.hex:main.c main.h gpio.rel i2c_bus.rel spi_bus.rel spi_sd.rel i2c_memory.rel LM032L.rel  one_wire_bus.rel ds18b20.rel ds1302.rel #spi_memory.rel #lcd12864-sed1565.rel 
+main.hex:main.c main.h gpio.rel spi_bus.rel spi_sd.rel one_wire_bus.rel ds18b20.rel #i2c_bus.rel i2c_memory.rel LM032L.rel ds1302.rel #spi_memory.rel #lcd12864-sed1565.rel 
 	sdcc $(opt) $^ -o main.ihx
 	packihx main.ihx >$@
 	
